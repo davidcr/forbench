dc69a86131b4e9e4766fc666ffdc566645b1a4b8
David Crescence <david.crescence@orange.com>
2019-01-07 11:25:30 +0100

    Bench for list.

--- app/src/test/java/davidcr/com/forbench/ExampleUnitTest.kt
+++ app/src/test/java/davidcr/com/forbench/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package davidcr.com.forbench
 
 import org.junit.Test
-
-import org.junit.Assert.*
+import kotlin.system.measureNanoTime
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +9,28 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun listIndexedTest() {
+        val list = (1..10000).toList()
+
+        println("ForLoop Time: " + measureNanoTime {
+            for (i in list) {
+                //do something
+            }
+        })
     }
+
+    @Test
+    fun listIteratorTest() {
+        val list = (1..10000).toList()
+
+        println("Iterator Time:" + measureNanoTime {
+            list.forEach {
+                //do something
+            }
+        })
+    }
+
+
 }
